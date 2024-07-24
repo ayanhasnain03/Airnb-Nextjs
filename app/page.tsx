cde6709f3123401953e9364f080107ba444a9e37
Ayan <ayan2572006@gmail.com>
2024-07-25 01:46:02 +0530

    listing api done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
 import Image from "next/image";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import getListing from "./actions/getListing";
 
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello Airnb</div>;
+export default async function Home() {
+  const listings = await getListing();
+  console.log(listings);
+  const isEmpty = true;
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          <div>My Future Listing</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
